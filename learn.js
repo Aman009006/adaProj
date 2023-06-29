498e864832d98b4b072ee028d843915010806e8d
Your Name <you@example.com>
2023-06-29 19:28:24 +0600

    added js

--- learn.js
+++ learn.js
@@ -1,4 +1,3 @@
-const button = document.getElementById("button");
 const burgerButton = document.getElementById("burder__open");
 const modal = document.getElementById("burder__modal");
 const closeModalButton = document.getElementById("close__modal");
